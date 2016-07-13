cb4416468ebca5334fe35052bb286a39d8a9b6aa
Tim Riley <tim@finergames.com>
2016-07-13 22:01:15 +0800

    - cleaned up entityview inspector - cleaned up some bugs in systems using HasComponent instead of GetComponent now - cleaned up events firing to nowhere in the enemy models - added ScoringSystem and ScoreView

---
+++ Assets/SurvivalShooter/Systems/IReactToDataSystem.cs
@@ -0,0 +1,17 @@
+using EcsRx.Entities;
+using EcsRx.Groups;
+using EcsRx.Systems;
+using EcsRx.Unity.Components;
+using UniRx;
+using UnityEngine;
+using EcsRx.Unity.MonoBehaviours;
+using EcsRx.Events;
+using System;
+
+namespace EcsRx.SurvivalShooter
+{
+	public interface IReactToDataSystem
+	{
+	}
+
+}
